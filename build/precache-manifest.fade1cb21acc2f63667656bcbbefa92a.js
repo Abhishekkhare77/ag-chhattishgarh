2c17be6ed9f190713564d2f00042a8b864b60b74
Abhishek khare <99396434+Abhishekkhare77@users.noreply.github.com>
2024-02-08 10:48:44 +0530

    Add files via upload

---
+++ build/precache-manifest.fade1cb21acc2f63667656bcbbefa92a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d005554eb8cd0a6af5a7ebd71b03e46e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ce0d0062634f3b3d985",
+    "url": "/static/css/2.41075ca0.chunk.css"
+  },
+  {
+    "revision": "c79fc456bb5d146e2cfd",
+    "url": "/static/css/main.a4086c8f.chunk.css"
+  },
+  {
+    "revision": "0ce0d0062634f3b3d985",
+    "url": "/static/js/2.a7ef1072.chunk.js"
+  },
+  {
+    "revision": "48f639e2c0e45902ee9838b578288017",
+    "url": "/static/js/2.a7ef1072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79fc456bb5d146e2cfd",
+    "url": "/static/js/main.549f0af7.chunk.js"
+  },
+  {
+    "revision": "ccd3c3e90d64a11dfcc7",
+    "url": "/static/js/runtime-main.a6f6b23b.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
